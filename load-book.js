e3089c1e97086351719534856804455585f39057
Kashyap Nasit <kashyapnasit@gmail.com>
2021-05-19 23:16:36 +0530

    Modify load-book.js

--- load-book.js
+++ load-book.js
@@ -1,24 +1,36 @@
-var $viewer = document.getElementById("viewer");
-var $next = document.getElementById("next");
-var $prev = document.getElementById("prev");
-var currentSection;
-var currentSectionIndex = 6;
+const $viewer = document.getElementById("viewer");
+const $next = document.getElementById("next");
+const $prev = document.getElementById("prev");
+let currentSection;
+let currentSectionIndex = 0;
 
-var book = ePub(
-	"hp.epub"
-);
+let currentPlaying = 0;
+
+const synth = window.speechSynthesis;
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector(".pitch-value");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector(".rate-value");
+
+const book = ePub("hp.epub");
 
 function extractContent(s) {
-	// console.log("Extracted called");
 	let span = document.createElement("span");
 	span.appendChild(s);
 	return span.textContent || span.innerText;
 }
 
-function onTextClick(event){
-    let id = event.target.id || event.target.parentNode.id;
-    let element = document.getElementById(id);
-    console.log(extractContent(element.cloneNode(true)));
+function play() {
+	let element = document.getElementById(currentPlaying);
+	const text = extractContent(element.cloneNode(true));
+	console.log(text);
+	speak(text);
+}
+
+function onTextClick(event) {
+	let id = event.target.id || event.target.parentNode.id;
+	currentPlaying = id;
+	play();
 }
 
 book.loaded.navigation.then(function (toc) {
@@ -68,20 +80,60 @@ book.loaded.navigation.then(function (toc) {
 		var section = book.spine.get(item);
 		if (section) {
 			currentSection = section;
-            section.load(this.book.load.bind(this.book)).then((contents) => {
-                $viewer.innerHTML = "";
-                $viewer.appendChild(contents.children[1]);
-                const listOfP = document.getElementsByTagName("p");
-                for(let i=0;i< listOfP.length;++i){
-                    listOfP[i].onclick = onTextClick;
-                }
+			section.load(this.book.load.bind(this.book)).then((contents) => {
+				$viewer.innerHTML = "";
+				$viewer.appendChild(contents.children[1]);
+				const listOfP = document.getElementsByTagName("p");
+				for (let i = 0; i < listOfP.length; ++i) {
+					listOfP[i].onclick = onTextClick;
+				}
 
-                const listOfSpan = document.getElementsByTagName("span");
-                for(let i=0;i< listOfSpan.length;++i){
-                    listOfSpan[i].onclick = onTextClick;
-                }
-            });
+				const listOfSpan = document.getElementsByTagName("span");
+				for (let i = 0; i < listOfSpan.length; ++i) {
+					listOfSpan[i].onclick = onTextClick;
+				}
+			});
 		}
 		return section;
 	}
 });
+
+function speak(data) {
+	if (synth.speaking) {
+		console.error("speechSynthesis.speaking");
+		return;
+	}
+	if (data !== "") {
+		// if (content !== "") {
+		var utterThis = new SpeechSynthesisUtterance(data);
+		// var utterThis = new SpeechSynthesisUtterance(content);
+		utterThis.onerror = function (event) {
+			console.log(
+				"An error has occurred with the speech synthesis: " +
+					event.error
+			);
+		};
+		utterThis.onend = function (event) {
+			console.log("SpeechSynthesisUtterance.onend");
+			playFrom(+_index + 1);
+		};
+		utterThis.onerror = function (event) {
+			console.error("SpeechSynthesisUtterance.onerror");
+		};
+
+		utterThis.voice = "en-US";
+		// voiceSelect.selectedOptions[0].getAttribute("data-name");
+		utterThis.pitch = pitch.value;
+		utterThis.rate = rate.value;
+        playSync();
+		async function playSync() {
+			synth.speak(utterThis);
+			return new Promise((resolve) => {
+				utterThis.onend = resolve;
+			});
+		}
+        currentPlaying++;
+        if(!synth.paused && !synth.speaking)
+            play();
+	}
+}
--- text-to-speach.js
+++ text-to-speach.js
@@ -1,112 +1,9 @@
+// var synth = window.speechSynthesis;
 
-
-function processText(text) {
-	console.log(text);
-	let content = text.split("\n");
-	let final = [];
-	for (let i = 0; i < content.length; ++i) {
-		const s = content[i].trim();
-		if (s.length > 0) {
-			final.push(s);
-		}
-	}
-	return final.join("<br>");
-}
-
-let content = "";
-let chapters = []
-let _index = 0;
-function load() {
-	content = "";
-	let HTMLpages = [];
-	this.book.loaded.spine
-		.then((spine) => {
-			spine.each((item) => {
-
-				item.load(this.book.load.bind(this.book)).then((contents) => {
-					HTMLpages.push(contents);
-					const text = processText(extractContent(contents));
-					content = content + "\n" + text;
-				});
-			});
-		})
-		.then(() => {
-			// speechSynthesis.speak(new SpeechSynthesisUtterance("content"));
-			// var inputTxt = document.querySelector(".txt");
-			// inputTxt.innerHTML = content;
-			const sentences = content.split(".");
-			let i = 0;
-			while (!synth.speaking && i < sentences.length) {
-				const body = document.getElementById("book");
-				const node = document.createElement("div");
-				node.innerHTML = sentences[i];
-				node.id = i;
-				node.onclick = playFrom;
-				body.appendChild(node);
-				i++;
-			}
-			console.log(content);
-		});
-}
-// function play() {
-// 	speechSynthesis.play();
-// }
-
-var synth = window.speechSynthesis;
-
-var inputForm = document.querySelector("form");
-var inputTxt = document.querySelector(".txt");
-var voiceSelect = document.querySelector("select");
-
-var pitch = document.querySelector("#pitch");
-var pitchValue = document.querySelector(".pitch-value");
-var rate = document.querySelector("#rate");
-var rateValue = document.querySelector(".rate-value");
-
-var voices = [];
-
-// function populateVoiceList() {
-// 	voices = synth.getVoices().sort(function (a, b) {
-// 		const aname = a.name.toUpperCase();
-// 		const bname = b.name.toUpperCase();
-// 		if (aname < bname) return -1;
-// 		else if (aname == bname) return 0;
-// 		else return +1;
-// 	});
-// 	var selectedIndex =
-// 		voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-// 	voiceSelect.innerHTML = "";
-// 	for (let i = 0; i < voices.length; i++) {
-// 		var option = document.createElement("option");
-// 		option.textContent = voices[i].name + " (" + voices[i].lang + ")";
-
-// 		if (voices[i].default) {
-// 			option.textContent += " -- DEFAULT";
-// 		}
-
-// 		option.setAttribute("data-lang", voices[i].lang);
-// 		option.setAttribute("data-name", voices[i].name);
-// 		voiceSelect.appendChild(option);
-// 	}
-// 	voiceSelect.selectedIndex = selectedIndex;
-// }
-
-// populateVoiceList();
-// if (
-// 	speechSynthesis.onvoiceschanged !== null ||
-// 	speechSynthesis.onvoiceschanged !== undefined
-// ) {
-// 	speechSynthesis.onvoiceschanged = populateVoiceList;
-// }
-
-function play() {
-	const sentences = content.split(".");
-	let i = 0;
-	while (!synth.speaking && i < sentences.length) {
-		speak(sentences[i]);
-		i++;
-	}
-}
+// var pitch = document.querySelector("#pitch");
+// var pitchValue = document.querySelector(".pitch-value");
+// var rate = document.querySelector("#rate");
+// var rateValue = document.querySelector(".rate-value");
 
 function playFrom(event) {
 	const index = event.target? event.target.id : event;
@@ -123,41 +20,36 @@ function playFrom(event) {
 	}
 }
 
-function speak(data) {
-	if (synth.speaking) {
-		console.error("speechSynthesis.speaking");
-		return;
-	}
-	if (data !== "") {
-		// if (content !== "") {
-		var utterThis = new SpeechSynthesisUtterance(data);
-		// var utterThis = new SpeechSynthesisUtterance(content);
-		utterThis.onerror = function (event) {
-			console.log(
-				"An error has occurred with the speech synthesis: " +
-					event.error
-			);
-		};
-		utterThis.onend = function (event) {
-			console.log("SpeechSynthesisUtterance.onend");
-			playFrom(+_index + 1);
-		};
-		utterThis.onerror = function (event) {
-			console.error("SpeechSynthesisUtterance.onerror");
-		};
-		var selectedOption = "en-US";
-		// voiceSelect.selectedOptions[0].getAttribute("data-name");
-		for (let i = 0; i < voices.length; i++) {
-			if (voices[i].name === selectedOption) {
-				utterThis.voice = voices[i];
-				break;
-			}
-		}
-		utterThis.pitch = pitch.value;
-		utterThis.rate = rate.value;
-		synth.speak(utterThis);
-	}
-}
+// function speak(data) {
+// 	if (synth.speaking) {
+// 		console.error("speechSynthesis.speaking");
+// 		return;
+// 	}
+// 	if (data !== "") {
+// 		// if (content !== "") {
+// 		var utterThis = new SpeechSynthesisUtterance(data);
+// 		// var utterThis = new SpeechSynthesisUtterance(content);
+// 		utterThis.onerror = function (event) {
+// 			console.log(
+// 				"An error has occurred with the speech synthesis: " +
+// 					event.error
+// 			);
+// 		};
+// 		utterThis.onend = function (event) {
+// 			console.log("SpeechSynthesisUtterance.onend");
+// 			playFrom(+_index + 1);
+// 		};
+// 		utterThis.onerror = function (event) {
+// 			console.error("SpeechSynthesisUtterance.onerror");
+// 		};
+	
+// 		utterThis.voice = "en-US";
+// 		// voiceSelect.selectedOptions[0].getAttribute("data-name");
+// 		utterThis.pitch = pitch.value;
+// 		utterThis.rate = rate.value;
+// 		synth.speak(utterThis);
+// 	}
+// }
 
 function pause() {
 	if (synth.paused) {
@@ -167,22 +59,10 @@ function pause() {
 	synth.pause();
 }
 
-inputForm.onsubmit = function (event) {
-	event.preventDefault();
-
-	speak();
-
-	inputTxt.blur();
-};
-
 pitch.onchange = function () {
 	pitchValue.textContent = pitch.value;
 };
 
 rate.onchange = function () {
 	rateValue.textContent = rate.value;
-};
-
-voiceSelect.onchange = function () {
-	speak();
 };
